5347fd08abd47cbd01cee066f17336e368096f34
dthree <threedeecee@gmail.com>
2015-08-25 11:32:34 -0700

    break and recreate color tags at line break

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 var spliceString = require('splice-string');
 
 module.exports = function (str, cols) {
+	var pre = '';
 	var ret = '';
 	var insideEscape = false;
+	var endCode = '\u001b[39m';
+	var escapeCode;
 	var visible = 0;
 	var lastSpace = 0;
 
 	for (var i = 0; i < str.length; i++) {
 		var x = str[i];
-
-		ret += x;
+		pre += x;
 
 		if (x === '\u001b') {
 			insideEscape = true;
@@ -28,9 +30,35 @@ module.exports = function (str, cols) {
 		}
 
 		if (++visible >= cols - 2 && lastSpace > 0) {
-			ret = spliceString(ret, lastSpace, 1, '\n');
+			pre = spliceString(pre, lastSpace, 1, '\n');
 			lastSpace = 0;
 			visible = 0;
+		}
+	}
+
+	visible = 0;
+	for (var j = 0; j < pre.length; j++) {
+		var y = pre[j];
+		ret += y;
+		if (y === '\u001b') {
+			var code = parseFloat(pre.slice(j, j + 4).match(/[0-9][^m]*/));
+			escapeCode = (code === 39) ? undefined : code;
+		} else if (insideEscape && y === 'm') {
+			insideEscape = false;
+			continue;
+		}
+
+		if (insideEscape) {
+			continue;
+		}
+
+		++visible;
+		if (escapeCode) {
+			if (pre[j + 1] === '\n') {
+				ret += endCode;
+			} else if (y === '\n') {
+				ret += '\u001b[' + escapeCode + 'm';
+			}
 		}
 	}
 
