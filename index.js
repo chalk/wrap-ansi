fe49e8cf62d07114c6d1b7b38076c9ad3081269d
root <root@cent-ws01.private.scn.net>
2015-08-27 11:36:27 -0700

    made 39 a constant

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var spliceString = require('splice-string');
 
 var controls = ['\u001b', '\u009b'];
-
+var endCode = 39;
 var sets = {
 	'0': 0,	'1': 22, '2': 22, '3': 23, '4': 24, '7': 27, '8': 28, '9': 2, '30': 39, '31': 39, '32': 39, '33': 39, '34': 39, '35': 39, '36': 39, '37': 39, '90': 3, '40': 49, '41': 49, '42': 49, '43': 49, '44': 49, '45': 49, '46': 49, '47': 4
 };
@@ -51,7 +51,7 @@ module.exports = function (str, cols) {
 		ret += y;
 		if (controls.indexOf(y) > -1) {
 			var code = parseFloat(/[0-9][^m]*/.exec(pre.slice(j, j + 4)));
-			escapeCode = (code === 39) ? undefined : code;
+			escapeCode = (code === endCode) ? undefined : code;
 		} else if (insideEscape && y === 'm') {
 			insideEscape = false;
 			continue;
