638aa00475180dfa930f33318c2d343f71053811
root <root@cent-ws01.private.scn.net>
2015-08-27 11:31:05 -0700

    made control chars a constant

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 var spliceString = require('splice-string');
 
+var controls = ['\u001b', '\u009b'];
+
 var sets = {
 	'0': 0,	'1': 22, '2': 22, '3': 23, '4': 24, '7': 27, '8': 28, '9': 2, '30': 39, '31': 39, '32': 39, '33': 39, '34': 39, '35': 39, '36': 39, '37': 39, '90': 3, '40': 49, '41': 49, '42': 49, '43': 49, '44': 49, '45': 49, '46': 49, '47': 4
 };
 
 function encode(code) {
-	return '\u001b[' + code + 'm';
+	return controls[0] + '[' + code + 'm';
 }
 
 module.exports = function (str, cols) {
@@ -21,7 +23,7 @@ module.exports = function (str, cols) {
 		var x = str[i];
 		pre += x;
 
-		if (x === '\u001b') {
+		if (controls.indexOf(x) > -1) {
 			insideEscape = true;
 		} else if (insideEscape && x === 'm') {
 			insideEscape = false;
@@ -47,7 +49,7 @@ module.exports = function (str, cols) {
 	for (var j = 0; j < pre.length; j++) {
 		var y = pre[j];
 		ret += y;
-		if (y === '\u001b') {
+		if (controls.indexOf(y) > -1) {
 			var code = parseFloat(/[0-9][^m]*/.exec(pre.slice(j, j + 4)));
 			escapeCode = (code === 39) ? undefined : code;
 		} else if (insideEscape && y === 'm') {
