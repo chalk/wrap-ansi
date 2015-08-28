16047ded1d1b9b186368b783e31f890bb69d22e9
dthree <threedeecee@gmail.com>
2015-08-28 13:57:02 +0700

    Close #1 PR: Break and re-create color tags at line break.

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict';
 var spliceString = require('splice-string');
 
+var controls = ['\u001b', '\u009b'];
+var endCode = 39;
+var sets = {
+	'0': 0,	'1': 22, '2': 22, '3': 23, '4': 24, '7': 27, '8': 28, '9': 2, '30': 39, '31': 39, '32': 39, '33': 39, '34': 39, '35': 39, '36': 39, '37': 39, '90': 3, '40': 49, '41': 49, '42': 49, '43': 49, '44': 49, '45': 49, '46': 49, '47': 4
+};
+
+function encode(code) {
+	return controls[0] + '[' + code + 'm';
+}
+
 module.exports = function (str, cols) {
+	var pre = '';
 	var ret = '';
 	var insideEscape = false;
+	var escapeCode;
 	var visible = 0;
 	var lastSpace = 0;
 
 	for (var i = 0; i < str.length; i++) {
 		var x = str[i];
+		pre += x;
 
-		ret += x;
-
-		if (x === '\u001b') {
+		if (controls.indexOf(x) > -1) {
 			insideEscape = true;
 		} else if (insideEscape && x === 'm') {
 			insideEscape = false;
@@ -28,9 +39,36 @@ module.exports = function (str, cols) {
 		}
 
 		if (++visible >= cols - 2 && lastSpace > 0) {
-			ret = spliceString(ret, lastSpace, 1, '\n');
+			pre = spliceString(pre, lastSpace, 1, '\n');
 			lastSpace = 0;
 			visible = 0;
+		}
+	}
+
+	visible = 0;
+	for (var j = 0; j < pre.length; j++) {
+		var y = pre[j];
+		ret += y;
+		if (controls.indexOf(y) > -1) {
+			var code = parseFloat(/[0-9][^m]*/.exec(pre.slice(j, j + 4)));
+			escapeCode = (code === endCode) ? undefined : code;
+		} else if (insideEscape && y === 'm') {
+			insideEscape = false;
+			continue;
+		}
+
+		if (insideEscape) {
+			continue;
+		}
+
+		visible++;
+
+		if (escapeCode && sets[escapeCode]) {
+			if (pre[j + 1] === '\n') {
+				ret += encode(sets[escapeCode]);
+			} else if (y === '\n') {
+				ret += encode(escapeCode);
+			}
 		}
 	}
 
--- test.js
+++ test.js
@@ -7,13 +7,17 @@ var fn = require('./');
 var fixture = 'The quick brown ' + chalk.red('fox jumped over ') + 'the lazy ' + chalk.green('dog and then ran away with the unicorn.');
 
 test(function (t) {
-	var res = fn(fixture, 20);
+	var res20 = fn(fixture, 20);
+	var res30 = fn(fixture, 30);
 
-	console.log(fn(fixture, 20) + '\n');
-
-	t.assert(res === 'The quick brown\n\u001b[31mfox jumped over \u001b[39mthe\nlazy \u001b[32mdog and then\nran away with the\nunicorn.\u001b[39m');
-	t.assert(stripAnsi(res).split('\n').every(function (x) {
+	t.assert(res20 === 'The quick brown\n\u001b[31mfox jumped over \u001b[39mthe\nlazy \u001b[32mdog and then\u001b[39m\n\u001b[32mran away with the\u001b[39m\n\u001b[32municorn.\u001b[39m');
+	t.assert(stripAnsi(res20).split('\n').every(function (x) {
 		return x.length <= 20;
+	}));
+
+	t.assert(res30 === 'The quick brown \u001b[31mfox jumped\u001b[39m\n\u001b[31mover \u001b[39mthe lazy \u001b[32mdog and then\u001b[39m\n\u001b[32mran away with the unicorn.\u001b[39m');
+	t.assert(stripAnsi(res30).split('\n').every(function (x) {
+		return x.length <= 30;
 	}));
 
 	t.end();
