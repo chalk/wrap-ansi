0c9ba676d56ade4f18fe8750c4b3f821aef8f01c
bcoe <bencoe@gmail.com>
2015-10-07 08:59:01 -0700

    switch to using \u001b based on @sindresorhus' review

--- test.js
+++ test.js
@@ -11,18 +11,18 @@ test(function (t) {
 	var res20 = fn(fixture, 20);
 	var res30 = fn(fixture, 30);
 
-	t.assert(res20 === 'The quick brown \x1B[31mfox\x1B[39m\n\x1B[31mjumped over \x1B[39mthe lazy\n\x1B[32mdog and then ran\x1B[39m\n\x1B[32maway with the\x1B[39m\n\x1B[32municorn.\x1B[39m');
+	t.assert(res20 === 'The quick brown \u001b[31mfox\u001b[39m\n\u001b[31mjumped over \u001b[39mthe lazy\n\u001b[32mdog and then ran\u001b[39m\n\u001b[32maway with the\u001b[39m\n\u001b[32municorn.\u001b[39m');
 	t.assert(stripAnsi(res20).split('\n').every(function (x) {
 		return x.length <= 20;
 	}));
 
-	t.assert(res30 === 'The quick brown \x1B[31mfox jumped\x1B[39m\n\x1B[31mover \x1B[39mthe lazy \x1B[32mdog and then ran\x1B[39m\n\x1B[32maway with the unicorn.\x1B[39m');
+	t.assert(res30 === 'The quick brown \u001b[31mfox jumped\u001b[39m\n\u001b[31mover \u001b[39mthe lazy \u001b[32mdog and then ran\u001b[39m\n\u001b[32maway with the unicorn.\u001b[39m');
 	t.assert(stripAnsi(res30).split('\n').every(function (x) {
 		return x.length <= 30;
 	}));
 
 	// words greater than 5 characters, e.g., unicorn., will be split onto multiple lines.
-	t.assert(res5 === 'The\nquick\nbrown\n\x1B[31mfox\x1B[39m\n\x1B[31mjumpe\x1B[39m\n\x1B[31md\x1B[39m\n\x1B[31mover\x1B[39m\n\x1B[31m\x1B[39mthe\nlazy\n\x1B[32mdog\x1B[39m\n\x1B[32mand\x1B[39m\n\x1B[32mthen\x1B[39m\n\x1B[32mran\x1B[39m\n\x1B[32maway\x1B[39m\n\x1B[32mwith\x1B[39m\n\x1B[32mthe\x1B[39m\n\x1B[32munico\x1B[39m\n\x1B[32mrn.\x1B[39m');
+	t.assert(res5 === 'The\nquick\nbrown\n\u001b[31mfox\u001b[39m\n\u001b[31mjumpe\u001b[39m\n\u001b[31md\u001b[39m\n\u001b[31mover\u001b[39m\n\u001b[31m\u001b[39mthe\nlazy\n\u001b[32mdog\u001b[39m\n\u001b[32mand\u001b[39m\n\u001b[32mthen\u001b[39m\n\u001b[32mran\u001b[39m\n\u001b[32maway\u001b[39m\n\u001b[32mwith\u001b[39m\n\u001b[32mthe\u001b[39m\n\u001b[32munico\u001b[39m\n\u001b[32mrn.\u001b[39m');
 	t.assert(stripAnsi(res5).split('\n').every(function (x) {
 		return x.length <= 5;
 	}));
