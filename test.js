08851585038fcf6bb2a336e45c95f96a8c5ced29
Kevin Martensson <kevinmartensson@gmail.com>
2017-07-21 21:22:28 +0200

    Remove moot comments

--- test.js
+++ test.js
@@ -86,12 +86,10 @@ test('no word-wrapping', t => {
 	t.is(res3, 'The q\nuick\nbrown\n\u001B[31mfox j\u001B[39m\n\u001B[31mumped\u001B[39m\n\u001B[31mover\u001B[39m\n\u001B[31m\u001B[39mthe l\nazy \u001B[32md\u001B[39m\n\u001B[32mog an\u001B[39m\n\u001B[32md the\u001B[39m\n\u001B[32mn ran\u001B[39m\n\u001B[32maway\u001B[39m\n\u001B[32mwith\u001B[39m\n\u001B[32mthe u\u001B[39m\n\u001B[32mnicor\u001B[39m\n\u001B[32mn.\u001B[39m');
 });
 
-// https://github.com/chalk/wrap-ansi/issues/10
 test('supports fullwidth characters', t => {
 	t.is(m('안녕하세', 4, {hard: true}), '안녕\n하세');
 });
 
-// https://github.com/chalk/wrap-ansi/issues/11
 test('supports unicode surrogate pairs', t => {
 	t.is(m('a\ud83c\ude00bc', 2, {hard: true}), 'a\n\ud83c\ude00\nbc');
 });
